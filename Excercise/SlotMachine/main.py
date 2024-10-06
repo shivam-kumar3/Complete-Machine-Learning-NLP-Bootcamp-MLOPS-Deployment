2217f28b6f600839ba9cfbb6680181b6e48ccc91
Shivam Kumar <shivamm.py@gmail.com>
2024-10-06 21:29:52 +0530

    new

--- Excercise/SlotMachine/main.py
+++ Excercise/SlotMachine/main.py
@@ -1,3 +1,7 @@
+MAX_LINE = 3
+MAX_BET = 100
+MIN_BET = 1
+
 def deposit():
     while True:
         try:
@@ -10,9 +14,33 @@ def deposit():
             print('Kindly enter digits only.')
     return amt
 
+def get_num_of_line():
+    while True:
+        try:
+            lines = int(input(f"Enter the number of lines to bet on (1 - {MAX_LINE})"))
+            if lines <= 1 or lines <= MAX_LINE:
+                break
+            else:
+                print("Please enter a number between the given lines option")
+        except ValueError as e:
+            print('Kindly enter digits only.')
+        
+    return lines
 
+def get_bet():
+    while True:
+        try:
+            amt = int(input("ENTER THE BETTING AMOUNT"))
+            if amt <= MIN_BET or amt  <= MAX_BET:
+                break
+            else:
+                print("Please enter a number between the given lines option")
+        except ValueError as e:
+            print('Kindly enter digits only.')
 
 def main():
     balance = deposit()
-
+    lines = get_num_of_line()
+    bet = get_bet()
+    print(balance,lines)
 main()
