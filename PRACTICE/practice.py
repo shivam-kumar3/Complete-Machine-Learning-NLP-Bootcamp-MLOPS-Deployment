715554eff8ff0f61823f8793e8434f02bbf1c571
Shivam Kumar <shivamm.py@gmail.com>
2024-11-08 23:48:23 +0530

    new que

--- PRACTICE/practice.py
+++ PRACTICE/practice.py
@@ -89,3 +89,15 @@ def factorial(n):
         return n * factorial(n - 1)
 
 print(factorial(5))
+
+
+
+def fibonacci(n):
+    # Base cases
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    # Recursive case
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
