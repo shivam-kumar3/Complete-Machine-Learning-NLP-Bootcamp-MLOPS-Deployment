cb37acd92e9b828a8340c30fc413e24aa2e54e44
Shivam Kumar <shivamm.py@gmail.com>
2024-11-12 21:46:46 +0530

    new

--- PRACTICE/loop_que.py
+++ PRACTICE/loop_que.py
@@ -23,3 +23,31 @@ sum = 0
 for i in range (1,11):
     sum += i 
 print(sum)
+
+
+# reverse a word
+# write a program to print the word python in reverse using a for loop
+
+word = 'python'
+for i in word[::-1]:
+    print(i, end= "")
+
+
+'''
+count vowels in a string 
+que - Write a programe to count the number of vowels in the word 'education'
+
+'''
+
+word = 'education'
+count = 0 
+vowels = 'aeiou'
+
+for i in word:
+    if i in vowels:
+        count += 1
+print(count)
+
+
+
+
