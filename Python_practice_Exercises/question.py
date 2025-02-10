6d065d61187a2cde1c8e3970430c69af9c98cee4
Shivam Kumar <shivamm.py@gmail.com>
2025-02-10 20:38:13 +0530

    new questions

---
+++ Python_practice_Exercises/question.py
@@ -0,0 +1,50 @@
+'''
+Exercise 1: Mailing Address
+Create a program that displays your name and complete mailing address. 
+The address should be printed in the format that is normally used in the area where you live. Your program does not need to read any input from the user.
+
+'''
+
+person1 = ['Shivam','Patna']
+
+print(f'Name: {person1[0]}\nAddress {person1[1]}')
+
+'''
+Exercise 2: Hello
+
+Write a program that asks the user to enter his or her name. The program should
+respond with a message that says hello to the user, using his or her name
+
+
+'''
+
+def greeting():
+    user = input("Enter your name:- ")
+    print(f"Hello, {user}!\n Have a nice day ahead")
+
+
+'''
+Exercise 3: Area of a Room
+Write a program that asks the user to enter the width and length of a room. Once these values have been read, your program should compute and display the area of the room. The length and the width will be entered as floating-point numbers. Include units in your prompt and output message; either feet or meters, depending on which unit you are more comfortable working with.
+
+
+'''
+
+def Area_calculator():
+    width = float(input("Enter the width of your room:- "))
+    length = float(input("Enter the length of your room:- "))
+    area = width * length
+
+    print(f"The total area of your room is {area:.2f} square meters")
+
+
+
+Area_calculator()
+
+'''
+Exercise 4: Area of a Field
+Create a program that reads the length and width of a farmer’s field from the user in
+feet. Display the area of the field in acres.
+
+'''
+
