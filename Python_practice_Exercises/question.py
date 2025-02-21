086482a558cc2a84158f3902d320f9d9d631d8a9
Shivam Kumar <shivamm.py@gmail.com>
2025-02-21 22:59:32 +0530

    new

--- Python_practice_Exercises/question.py
+++ Python_practice_Exercises/question.py
@@ -366,3 +366,33 @@ if a + b > c and a + c > b and b + c > a:
         print("The triangle is Scalene.")
 else:
     print("The given sides do not form a valid triangle.")
+
+
+'''
+Exercise 58: Is It a Leap Year?
+(Solved, 22 Lines)
+Most years have 365 days. However, the time required for the Earth to orbit the Sun
+is actually slightly more than that. As a result, an extra day, February 29, is included
+in some years to correct for this difference. Such years are referred to as leap years.
+The rules for determining whether or not a year is a leap year follow:
+• Any year that is divisible by 400 is a leap year.
+• Of the remaining years, any year that is divisible by 100 is not a leap year.
+• Of the remaining years, any year that is divisible by 4 is a leap year.
+• All other years are not leap years.
+Write a program that reads a year from the user and displays a message indicating
+whether or not it is a leap year.
+
+'''
+
+# Get user input
+year = int(input("Enter a year: "))
+
+# Check leap year conditions
+if year % 400 == 0:
+    print(f"{year} is a leap year.")
+elif year % 100 == 0:
+    print(f"{year} is not a leap year.")
+elif year % 4 == 0:
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
