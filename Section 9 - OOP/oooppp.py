fcf4d840f5aa6ce676dfa8744eaa3b0d26036203
Shivam Kumar <shivamm.py@gmail.com>
2025-06-13 23:24:48 +0530

    updated

---
+++ Section 9 - OOP/oooppp.py
@@ -0,0 +1,22 @@
+# 1st approach - Impretive approach
+a = 6
+b =  19
+
+print(a+b)
+
+#2nd approach - Functional approach
+
+def add (a,b):
+    print(a+b)
+
+add(3,2)
+
+
+# OOP  is a programming paradigm based on the concept of objects which can contain data attributes and code(methods)
+
+class Addition:
+    def __init__(self,a,b):
+        print(a+b)
+
+obj = Addition(12,34)
+obj
