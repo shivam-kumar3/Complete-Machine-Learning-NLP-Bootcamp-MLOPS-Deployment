e0cba53347650fc14853624a95bc6b07edef9b2d
Shivam Kumar <shivamm.py@gmail.com>
2025-06-27 23:00:28 +0530

    new

--- g2g_que.py
+++ g2g_que.py
@@ -57,3 +57,45 @@ print(a.split())
 for i in a.split():
     print(i)
 
+# find the length of a given string without using the len()function
+
+def length(str):
+    size = 0
+    for i in str:
+        size += 1 
+
+    print(size)
+
+
+length('shivaaaaaaaaaaaa')
+
+
+# extract username from a given email
+#  if the email is shivam@gmail.com then the username should be shivam
+
+
+def username(email):
+    user = ""
+    for i in email:
+        if i == "@":
+            break
+        else:
+            user += i
+    print(user)
+
+
+username('shivamkumar1957@gmail.com')
+
+
+# write a program that can check whether a given string is palindrome or not
+
+def checker(str):
+    if str == str[::-1]:
+        print('String is palindrome')
+    else:
+        print('String is not palindrom')
+
+
+checker('shivam')
+checker('wow')
+checker('malayalam')
